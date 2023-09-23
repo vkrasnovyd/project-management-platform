d777c3a399fca7b23a739e1be350c4b9fe42598c
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-23 15:46:20 +0200

    Add Position and TaskType models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
