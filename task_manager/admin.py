6435184b13bef20837eb3317d811dd264a802db0
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 17:29:29 +0200

    Register Project model in admin view

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 
-from task_manager.models import Position, TaskType
+from task_manager.models import Position, TaskType, Project
 
 admin.site.register(Position)
 
 admin.site.register(TaskType)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active")
+    search_fields = ("name",)
+    list_filter = ("is_active",)
