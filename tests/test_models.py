34af5ff7a6982d1d30dcb4b0bf6ceb565872fa8e
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 16:07:37 +0200

    Add test for Task str method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from task_manager.models import Position, TaskType, Project
+from task_manager.models import Position, TaskType, Project, Task
 
 
 class PositionModelTest(TestCase):
@@ -37,3 +37,42 @@ class ProjectModelTest(TestCase):
             name="NewClient website"
         )
         self.assertEqual(str(project), project.name)
+
+
+class TaskModelTest(TestCase):
+    def test_task_str(self):
+        position1 = Position.objects.create(name="Project manager")
+        position2 = Position.objects.create(name="Copywriter")
+        project_manager = get_user_model().objects.create(
+            first_name="Jack",
+            last_name="Smith",
+            username="jack.smith",
+            password="Test1234",
+            position=position1
+        )
+        copywriter = get_user_model().objects.create(
+            first_name="John",
+            last_name="Doe",
+            username="john.doe",
+            password="Test1234",
+            position=position2
+        )
+        task_type = TaskType.objects.create(name="Copywriting")
+        project = Project.objects.create(
+            name="NewClient website"
+        )
+
+        task = Task.objects.create(
+            name="NewClient - Text for 'About us' page",
+            project=project,
+            deadline="2023-11-12",
+            description="""
+            Hi, John!
+            The client has filled in our form with the info about the company: https://example.com/about-us-brief.
+            Check his answers as soon as you can and let me know if you need any extra info for 'About us' text.
+            """,
+            author=project_manager,
+            responsible=copywriter,
+            task_type=task_type
+        )
+        self.assertEqual(str(task), task.name)
