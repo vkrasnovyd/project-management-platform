acac4bb19dbc38cf6057daa06e0c4a455f4bbb3a
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 17:33:36 +0200

    Register Task model in admin view

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from task_manager.models import Position, TaskType, Project
+from task_manager.models import Position, TaskType, Project, Task
 
 admin.site.register(Position)
 
@@ -12,3 +12,17 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ("name", "is_active")
     search_fields = ("name",)
     list_filter = ("is_active",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "task_type",
+        "is_completed",
+        "status", "deadline",
+        "author",
+        "responsible"
+    )
+    search_fields = ("name", "author", "responsible")
+    list_filter = ("is_completed", "status", "task_type")
