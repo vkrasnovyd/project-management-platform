b4ac7f0233c33124b983eae7ce7e9a9e13fc2e68
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 15:20:29 +0200

    Add test for Worker str method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from task_manager.models import Position, TaskType
@@ -13,3 +14,18 @@ class TaskTypeModelTest(TestCase):
     def test_task_type_str(self):
         task_type = TaskType.objects.create(name="New feature")
         self.assertEqual(str(task_type), task_type.name)
+
+
+class WorkerModelTest(TestCase):
+    def test_worker_str(self):
+        position = Position.objects.create(name="Python Developer")
+        worker = get_user_model().objects.create(
+            first_name="John",
+            last_name="Doe",
+            password="Test1234",
+            position=position
+        )
+        self.assertEqual(
+            str(worker),
+            f"{worker.first_name} {worker.last_name}"
+        )
