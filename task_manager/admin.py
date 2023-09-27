eb4c170766f07a4b278fd69eaa3e211188da7827
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 17:09:40 +0200

    Register Position & TaskType models in admin view

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_manager.models import Position, TaskType
+
+admin.site.register(Position)
+
+admin.site.register(TaskType)
