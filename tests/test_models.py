3386ce8a05773643e5b56cbd3165c1b807ba0151
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-27 15:15:45 +0200

    Add test for TaskType str method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
-from task_manager.models import Position
+from task_manager.models import Position, TaskType
 
 
 class PositionModelTest(TestCase):
     def test_position_str(self):
         position = Position.objects.create(name="Python Developer")
         self.assertEqual(str(position), position.name)
+
+
+class TaskTypeModelTest(TestCase):
+    def test_task_type_str(self):
+        task_type = TaskType.objects.create(name="New feature")
+        self.assertEqual(str(task_type), task_type.name)
