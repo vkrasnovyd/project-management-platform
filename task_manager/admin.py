28aeb20e6b7de0f94084f133bdd3caeae61da829
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-09-28 21:52:04 +0200

    Register Worker(User) model in admin view

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,10 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from task_manager.models import Position, TaskType, Project, Task
+from task_manager.models import Position, TaskType, Project, Task, Worker
 
 admin.site.register(Position)
 
 admin.site.register(TaskType)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    list_filter = UserAdmin.list_filter + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info", {
+                "fields": (
+                    "position",
+                )
+            }
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "position"
+                )
+            },
+        ),
+    )
 
 
 @admin.register(Project)
