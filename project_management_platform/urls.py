ecfce13dfd145b0518d4e50bda0e854b204a8d21
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-12-06 20:45:38 +0100

    Implement Login system

--- project_management_platform/urls.py
+++ project_management_platform/urls.py
@@ -22,4 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("task_manager.urls", namespace="task_manager")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    return render(request, "task_manager/index.html")
