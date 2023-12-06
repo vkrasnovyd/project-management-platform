61ec630e3e0143062a55ed175f4b6bc5cf1246df
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2023-12-06 20:59:08 +0100

    Add WorkerListView and WorkerDetailView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from task_manager.views import (
     index,
+    WorkerListView,
+    WorkerDetailView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
 ]
 
 app_name = "task_manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+
+from task_manager.forms import WorkerCreationForm
+from task_manager.models import Task
 
 
 @login_required
@@ -7,3 +13,29 @@ def index(request):
     """View function for the home page of the site."""
 
     return render(request, "task_manager/index.html")
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    """View class for the page with a list of all workers grouped by position."""
+
+    model = get_user_model()
+    paginate_by = 20
+    queryset = get_user_model().objects.select_related("position").all()
+
+
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
+    """View class for the page with the key information about a worker."""
+
+    model = get_user_model()
+
+    def get_context_data(self, **kwargs):
+        context = super(WorkerDetailView, self).get_context_data()
+        user = self.request.user
+        responsible = context.get("worker")
+        common_tasks_in_progress = Task.objects.filter(
+            author=user,
+            responsible=responsible,
+            is_completed=False
+        ).order_by("deadline")
+        context["common_tasks_in_progress"] = common_tasks_in_progress
+        return context
