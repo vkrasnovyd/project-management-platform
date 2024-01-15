eb3ec4ae334883c146633d52d5c52a1dadee926a
Viktoria Krasnovyd <v.krasnovyd@gmail.com>
2024-01-15 14:33:30 +0100

    Update staticfiles management for production

--- project_management_platform/settings.py
+++ project_management_platform/settings.py
@@ -143,11 +143,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "staticfiles/"
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Assets Management
 ASSETS_ROOT = "/static/assets"
